3697d4ac85acce03d7f811c95e5d9c096e5763cf
CarlosPatterson <15373516-CarlosPatterson@users.noreply.replit.com>
2024-02-11 11:17:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,72 @@
+if 5 > 2:
+  print("Five is greater than two!")
+ #   print("Five is greater than two!") indentation error single condition must be is same line
+if 5 > 2:
+  print("Five is greater than two!")
+
+x = 5
+y = "Hello, World!"
+print(x,y)
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+
+print("Hello, world!")
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+# Casting
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+# Get the type
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+
+# Single or Double Quotes?
+
+x = "John"
+# is the same as 
+x = 'John'
+
+
+# Case-Sensitive
+
+a = 4
+A = "Sally"
+# A will not overwrite a
+
+# Python - Variable Names
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+# Camel Case
+myVariableName = "John"
+
+# Pascal Case 
+MyVariableName = "John"
+
+# Snake Case
+my_variable_name = "John"
+
